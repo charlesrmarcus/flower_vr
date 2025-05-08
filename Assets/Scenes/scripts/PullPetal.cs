2023a369b1ce5d8a3deba8bcaaf54f668ca6ae1f
Charles Marcus <charlesrmarcus@gmail.com>
2025-05-08 13:19:04 -0500

    dissemble script v1

---
+++ Assets/Scenes/scripts/PullPetal.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
+
+
+[RequireComponent(typeof(UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable))]
+
+public class BreakApartOnActivate : MonoBehaviour
+{
+	[Header("Tuning")]
+	[SerializeField] float explosionForce = 3f;
+    [SerializeField] float explosionRadius = 0.5f;
+
+    public Component[] petals;
+
+    UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grab;
+
+    //void Awake() => grab = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+
+    //void OnEnable() => grab.activated += OnActivate;
+    //void OnDisable() => grab.activated -= OnActivate;
+
+    void Update() // trigger pulled
+    {
+        petals = GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody rb in petals)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                rb.isKinematic = false;
+                rb.useGravity = true;
+                rb.transform.parent = null;
+                rb.AddExplosionForce(explosionForce,
+                                     grab.transform.position,
+                                     explosionRadius,
+                                     0.1f);
+            }
+        }
+    }
+
+
+    //void OnActivate(ActivateEventArgs _) // trigger pulled
+	//{
+	//	petals = GetComponentsInChildren<Rigidbody>();
+
+    //    foreach (Rigidbody rb in petals)
+	//	{
+			//if (rb == grab.attachedRigidbody) continue;
+	//		rb.isKinematic = false;
+	//		rb.useGravity = true;
+	//		rb.transform.parent = null;
+	//		rb.AddExplosionForce(explosionForce,
+	//							 grab.transform.position,
+	//							 explosionRadius,
+	//							 0.1f);
+	//	}
+	//}
+}
