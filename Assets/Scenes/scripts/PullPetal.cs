86b7211505a2461bfea241fb35e13c60415dc8a3
Charles Marcus <charlesrmarcus@gmail.com>
2025-05-31 13:11:59 -0500

    Background music added

--- Assets/Scenes/scripts/PullPetal.cs
+++ Assets/Scenes/scripts/PullPetal.cs
@@ -27,7 +27,7 @@ public class BreakApartOnActivate : MonoBehaviour
     {
         flower = GetComponent<XRGrabInteractable>();
         petals = flower.GetComponentsInChildren<Rigidbody>();
-        petalCounter = 3;
+        petalCounter = 2;
         pullPetalAction.action.Enable();
         pullPetalAction.action.performed += OnActivate;
     }
